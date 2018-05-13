9d1ee93ad688c082ce7346c667baba87a5401453
ManuHeel <mrmanuheel@hotmail.fr>
2018-05-13 20:02:19 +0200

    YOLOOOOOOOOOOOOOO

--- Programmed_Unit_of_Chiptune_Ensemble/src/main.cpp
+++ Programmed_Unit_of_Chiptune_Ensemble/src/main.cpp
@@ -1,3 +1,4 @@
+// Libraries and sub-files
 #include <Arduino.h>
 #include <MIDI.h>
 #include "pitches.h"
@@ -14,67 +15,215 @@ const int AUDIO_OUTPUT_PIN = A14;
 // MIDI settings
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
-// Waveform arrays
-int SQUARE_WAVE [16] = { 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255 };
+// Wavetables
+uint8_t SQUARE_1_WAVETABLE [16] = { 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255 };
+uint8_t SQUARE_2_WAVETABLE [16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255 };
+uint8_t SQUARE_3_WAVETABLE [16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255 };
+uint8_t TRIANGLE_WAVETABLE [16] = { 127, 159, 191, 223, 255, 223, 191, 159, 127, 95, 63, 31, 0, 31, 63, 95 };
+uint8_t SAWTOOTH_WAVETABLE [16] = { 127, 143, 159, 175, 191, 207, 223, 239, 0, 15, 31, 47, 63, 79, 95, 111 };
+uint8_t SINE_WAVETABLE [16] = { 128, 176, 218, 245, 255, 245, 218, 176, 128, 79, 37, 10, 0, 10, 37, 79 };
+
+// Enums
+enum Waveform { SQUARE_1, SQUARE_2, SQUARE_3, TRIANGLE, SAWTOOTH, SINE };
+enum PolyphonyMode { MONO, POLY, ARP, SLIDE };
 
 // Oscillators
-volatile unsigned short OSC1_PHASE = 0;
-volatile unsigned short OSC1_VALUE = 127;
-volatile unsigned long OSC1_GAIN = 0;
+struct Oscillator {
+  uint8_t index; // Main identifier
+  volatile unsigned short phase ;// Current phase (updates on every period defined by the note chosen)
+  volatile unsigned short value; // Current value to output (waveform shifted by phase)
+  volatile float gain; // Gain value (0 to 1)
+  bool active; // Is the oscillator currently used?
+  Waveform waveform; // Waveform identifier
+  uint8_t* wavetable; // Pointer to wavetable
+} osc1, osc2, osc3;
 
-volatile unsigned short OSC2_PHASE = 0;
-volatile unsigned short OSC2_VALUE = 127;
-volatile unsigned long OSC2_GAIN = 0;
-
-volatile unsigned short OSC3_PHASE = 0;
-volatile unsigned short OSC3_VALUE = 127;
-volatile unsigned long OSC3_GAIN = 0;
+// Keyboards
+struct Keyboard{
+  Waveform waveform; // Waveform identifier
+  PolyphonyMode polyphonyMode; // Polyphony mode identifier
+  uint8_t activeKeys[10] = {0,0,0,0,0,0,0,0,0,0}; // Current active keys (i.e. pressed keys)
+  uint8_t nextKeyIndex = 0; // Index to next free active key. Back to 0 if full.
+  uint8_t oscillatorIndex; // Index identifying the reserved oscillator (in MONO)
+} upperKeyboard, lowerKeyboard, pedalBoard;
 
 // Timer based functions
 void processAudio() {
-  uint8_t divider = OSC1_GAIN + OSC2_GAIN + OSC3_GAIN; // Divide the master gain by the sum of each gain
-  uint16_t outputValue = (OSC1_VALUE*OSC1_GAIN + OSC2_VALUE*OSC2_GAIN + OSC3_VALUE*OSC3_GAIN) / divider; // Compute the current audio signal to output
+  //double divider = osc1.gain + osc2.gain + osc3.gain; // Divide the master gain by the sum of each gain // MEH
+  double divider = 3;
+  uint16_t outputValue = (double) (osc1.value*osc1.gain + osc2.value*osc2.gain + osc3.value*osc3.gain) / divider; // Compute the current audio signal to output
   analogWrite(AUDIO_OUTPUT_PIN, outputValue);
 }
-void updateOsc1() {
-  OSC1_VALUE = SQUARE_WAVE[OSC1_PHASE];
-  if (OSC1_PHASE < 15) OSC1_PHASE++; else OSC1_PHASE = 0;
+void processOsc1() {
+  osc1.value = osc1.wavetable[osc1.phase];
+  if (osc1.phase < 15) osc1.phase++; else osc1.phase = 0;
 }
-void updateOsc2() {
-  OSC2_VALUE = SQUARE_WAVE[OSC2_PHASE];
-  if (OSC2_PHASE < 15) OSC2_PHASE++; else OSC2_PHASE = 0;
+void processOsc2() {
+  osc2.value = osc2.wavetable[osc2.phase];
+  if (osc2.phase < 15) osc2.phase++; else osc2.phase = 0;
 }
-void updateOsc3() {
-  OSC3_VALUE = SQUARE_WAVE[OSC3_PHASE];
-  if (OSC3_PHASE < 15) OSC3_PHASE++; else OSC3_PHASE = 0;
-}
-
-void playNote(uint8_t midiNote, uint8_t midiChannel) {
-  if (midiChannel == 1) {
-    OSC1_GAIN = 1;
-    OSC1_TIMER.begin(updateOsc1, pitchToPeriod(midiNote));
-  } else if (midiChannel == 2) {
-    OSC2_GAIN = 1;
-    OSC2_TIMER.begin(updateOsc2, pitchToPeriod(midiNote));
-  } 
+void processOsc3() {
+  osc3.value = osc3.wavetable[osc3.phase];
+  if (osc3.phase < 15) osc3.phase++; else osc3.phase = 0;
 }
 
-void stopNote(uint8_t midiNote, uint8_t midiChannel) {
-  if (midiChannel == 1) {
-    OSC1_GAIN = 0;
-  } else if (midiChannel == 2) {
-    OSC2_GAIN = 0;
-  } 
+void updateOscillator(Oscillator& oscillator, float period, Waveform waveform, uint8_t gain) {
+  if(gain > 0) {
+    if (waveform != oscillator.waveform) {
+      oscillator.waveform = waveform;
+      switch (waveform) {
+        case SQUARE_1:
+          oscillator.wavetable = SQUARE_1_WAVETABLE;
+          break;
+        case SQUARE_2:
+          oscillator.wavetable = SQUARE_2_WAVETABLE;
+          break;
+        case SQUARE_3:
+          oscillator.wavetable = SQUARE_3_WAVETABLE;
+          break;
+        case TRIANGLE:
+          oscillator.wavetable = TRIANGLE_WAVETABLE;
+          break;
+        case SAWTOOTH:
+          oscillator.wavetable = SAWTOOTH_WAVETABLE;
+          break;
+        case SINE:
+          oscillator.wavetable = SINE_WAVETABLE;
+          break;
+      }
+    }
+    switch (oscillator.index) {
+      case 1:
+        OSC1_TIMER.begin(processOsc1, period); // Set the frequency (1/period)
+        break;
+      case 2:
+        OSC2_TIMER.begin(processOsc2, period); // Set the frequency (1/period)
+        break;
+      case 3:
+        OSC3_TIMER.begin(processOsc1, period); // Set the frequency (1/period)
+        break;
+    }
+    oscillator.active = true;
+  } else {
+    oscillator.active = false;
+  }
+  oscillator.gain = gain;
+}
+
+void addNote(uint8_t midiNote, Keyboard& keyboard) {
+  switch (keyboard.polyphonyMode) {
+    case MONO:
+      if (keyboard.nextKeyIndex <= 9) { // If there is still room for a new key
+        keyboard.activeKeys[keyboard.nextKeyIndex] = midiNote; // Add the new note at the end of the active keys
+        keyboard.nextKeyIndex++; // Update the index of the end of the active keys
+        switch (keyboard.oscillatorIndex) {
+          case 1:
+            updateOscillator(osc1, keyToPeriod(midiNote), keyboard.waveform, 1); // Ask the oscillator to play the very new note
+            break;
+          case 2:
+            updateOscillator(osc2, keyToPeriod(midiNote), keyboard.waveform, 1); // Ask the oscillator to play the very new note
+            break;
+          case 3:
+            updateOscillator(osc3, keyToPeriod(midiNote), keyboard.waveform, 1); // Ask the oscillator to play the very new note
+            break;
+        }
+      }
+      break;
+  }
+}
+
+void removeNote(uint8_t midiNote, Keyboard& keyboard) {
+  switch (keyboard.polyphonyMode) {
+    case MONO:
+      /* Remove the element from the array */
+      uint8_t noteToRemoveIndex = 255; // Note to remove (255 while it has not been found)
+      int i = 0; // Main iterator
+      while (i < 10) { // As long as we haven't reached the end nor found the note to remove...
+        if (keyboard.activeKeys[i] == midiNote) {
+          noteToRemoveIndex = i; // The note to remove has been found
+          break;
+        }
+        i++;
+      }
+      bool isTheCurrentNote = false;
+      if (noteToRemoveIndex == keyboard.nextKeyIndex-1) isTheCurrentNote = true; // If the note to remove was the last, we'll have to play the previous note, if it was not, we won't change the current note played
+      for (i = noteToRemoveIndex; i < keyboard.nextKeyIndex; i++) { // From the note to remove to the end of the array...
+        keyboard.activeKeys[i] = keyboard.activeKeys[i+1]; // Shift each element 1 row left
+      }
+      for (i = keyboard.nextKeyIndex; i < 10; i++) { // From the note to remove to the end of the array...
+        keyboard.activeKeys[i] = 0; // Shift each element 1 row left
+      }
+      keyboard.nextKeyIndex--; // Update the index of the end of the active keys
+
+      if (keyboard.nextKeyIndex != 0 && isTheCurrentNote) { // If there still is an active note to play and we did remove the very current playing one
+        switch (keyboard.oscillatorIndex) {
+          case 1:
+            updateOscillator(osc1, keyToPeriod(keyboard.activeKeys[keyboard.nextKeyIndex-1]), keyboard.waveform, 1); // Ask the oscillator to play the last played note still active
+            break;
+          case 2:
+            updateOscillator(osc2, keyToPeriod(keyboard.activeKeys[keyboard.nextKeyIndex-1]), keyboard.waveform, 1); // Ask the oscillator to play the last played note still active
+            break;
+          case 3:
+            updateOscillator(osc3, keyToPeriod(keyboard.activeKeys[keyboard.nextKeyIndex-1]), keyboard.waveform, 1); // Ask the oscillator to play the last played note still active
+            break;
+        }
+      } else if (keyboard.nextKeyIndex == 0) { // Else if it was the last one possible to play 
+        switch (keyboard.oscillatorIndex) {
+          case 1:
+            updateOscillator(osc1, 0, keyboard.waveform, 0); // Ask the oscillator to stop
+            break;
+          case 2:
+            updateOscillator(osc2, 0, keyboard.waveform, 0); // Ask the oscillator to stop
+            break;
+          case 3:
+            updateOscillator(osc3, 0, keyboard.waveform, 0); // Ask the oscillator to stop
+            break;
+        }
+      }
+      break;
+  }
 }
 
 void setup() {
-  pinMode(AUDIO_OUTPUT_PIN, OUTPUT);
-  MIDI.begin(MIDI_CHANNEL_OMNI);
-  AUDIO_OUTPUT_TIMER.begin(processAudio, 20);  // Audio output processing at roughly 48kHz 440Hz
-  OSC1_TIMER.begin(updateOsc1, 0);  // Set frequency of the oscillator to roughly 440Hz
-  OSC2_TIMER.begin(updateOsc2, 0);  // Set frequency of the oscillator to roughly 220Hz
-  OSC3_TIMER.begin(updateOsc3, 0);  // Set frequency of the oscillator to roughly 330Hz
-  playNote(0x39, 1);
+  // Main setup
+  Serial.begin(9600); // USB is always 12 Mbit/sec
+  pinMode(AUDIO_OUTPUT_PIN, OUTPUT); // Initialize the DAC
+  AUDIO_OUTPUT_TIMER.begin(processAudio, 20);  // Audio output processing at roughly 48kHz
+  // Initialize the oscillators
+  osc1.index = 1;
+  osc1.active = false;
+  osc1.phase = 0;
+  osc1.gain = 0;
+  osc1.value = 127;
+  osc1.waveform = SQUARE_1;
+  osc1.wavetable = SQUARE_1_WAVETABLE;
+  osc2.index = 2;
+  osc2.active = false;
+  osc2.phase = 0;
+  osc2.gain = 0;
+  osc2.value = 127;
+  osc2.waveform = SQUARE_1;
+  osc2.wavetable = SQUARE_1_WAVETABLE;
+  osc3.index = 3;
+  osc3.active = false;
+  osc3.phase = 0;
+  osc3.gain = 0;
+  osc3.value = 127;
+  osc3.waveform = SQUARE_1;
+  osc3.wavetable = SQUARE_1_WAVETABLE;
+  // Initialize the keyboards
+  upperKeyboard.waveform = SQUARE_2;
+  upperKeyboard.polyphonyMode = MONO;
+  upperKeyboard.oscillatorIndex = 1;
+  lowerKeyboard.waveform = SAWTOOTH;
+  lowerKeyboard.polyphonyMode = MONO;
+  lowerKeyboard.oscillatorIndex = 2;
+  pedalBoard.waveform = TRIANGLE;
+  pedalBoard.polyphonyMode = MONO;
+  pedalBoard.oscillatorIndex = 3;
+  // Wait a bit to ignore strange initialization messages from the MIDI module, then start the MIDI communication
+  delay(3000);
+  MIDI.begin(MIDI_CHANNEL_OMNI); // Initialize MIDI communications
 }
 
 // The main program
@@ -83,22 +232,45 @@ void loop() {
   int midiType, midiNote, midiVelocity, midiChannel, midiD1, midiD2;
   if (MIDI.read()) { // If there is a MIDI message incoming
     midiType = MIDI.getType();
+    Keyboard* keyboard;
     switch (midiType) {
       case midi::NoteOn:
         midiNote = MIDI.getData1();
         midiVelocity = MIDI.getData2();
         midiChannel = MIDI.getChannel();
+        switch (midiChannel) {
+          case 1:
+            keyboard = &upperKeyboard;
+            break;
+          case 2:
+            keyboard = &lowerKeyboard;
+            break;
+          case 3:
+            keyboard = &pedalBoard;
+            break;
+        }
         if (midiVelocity > 0) {
-          playNote(midiNote, midiChannel);
+          addNote(midiNote, *keyboard);
         } else {
-          stopNote(midiNote, midiChannel);
+          removeNote(midiNote, *keyboard);
         }
         break;
       case midi::NoteOff:
         midiNote = MIDI.getData1();
         midiVelocity = MIDI.getData2();
         midiChannel = MIDI.getChannel();
-        stopNote(midiNote, midiChannel);
+        switch (midiChannel) {
+          case 1:
+            keyboard = &upperKeyboard;
+            break;
+          case 2:
+            keyboard = &lowerKeyboard;
+            break;
+          case 3:
+            keyboard = &pedalBoard;
+            break;
+        }
+        removeNote(midiNote, *keyboard);
         break;
       default:
         midiD1 = MIDI.getData1();
--- Programmed_Unit_of_Chiptune_Ensemble/src/pitches.h
+++ Programmed_Unit_of_Chiptune_Ensemble/src/pitches.h
@@ -1,127 +1,127 @@
-// Converts a MIDI pitch to a A440-tuned 16-bit period
-float pitchToPeriod(int pitch) {
+// Converts a MIDI key to a A440-tuned 16-bit period
+float keyToPeriod(int key) {
   float period = 142.05;
-  if (pitch == 0x18) {
+  if (key == 0x18) {
     period = 955.57;
-  } else if (pitch == 0x19) {
+  } else if (key == 0x19) {
     period = 901.93;
-  } else if (pitch == 0x1A) {
+  } else if (key == 0x1A) {
     period = 851.31;
-  } else if (pitch == 0x1B) {
+  } else if (key == 0x1B) {
     period = 803.53;
-  } else if (pitch == 0x1C) {
+  } else if (key == 0x1C) {
     period = 758.43;
-  } else if (pitch == 0x1D) {
+  } else if (key == 0x1D) {
     period = 715.86;
-  } else if (pitch == 0x1E) {
+  } else if (key == 0x1E) {
     period = 675.68;
-  } else if (pitch == 0x1F) {
+  } else if (key == 0x1F) {
     period = 637.76;
-  } else if (pitch == 0x20) {
+  } else if (key == 0x20) {
     period = 601.95;
-  } else if (pitch == 0x21) {
+  } else if (key == 0x21) {
     period = 568.18;
-  } else if (pitch == 0x22) {
+  } else if (key == 0x22) {
     period = 536.3;
-  } else if (pitch == 0x23) {
+  } else if (key == 0x23) {
     period = 506.2;
-  } else if (pitch == 0x24) {
+  } else if (key == 0x24) {
     period = 477.79;
-  } else if (pitch == 0x25) {
+  } else if (key == 0x25) {
     period = 450.97;
-  } else if (pitch == 0x26) {
+  } else if (key == 0x26) {
     period = 425.66;
-  } else if (pitch == 0x27) {
+  } else if (key == 0x27) {
     period = 401.77;
-  } else if (pitch == 0x28) {
+  } else if (key == 0x28) {
     period = 379.22;
-  } else if (pitch == 0x29) {
+  } else if (key == 0x29) {
     period = 357.94;
-  } else if (pitch == 0x2A) {
+  } else if (key == 0x2A) {
     period = 337.84;
-  } else if (pitch == 0x2B) {
+  } else if (key == 0x2B) {
     period = 318.88;
-  } else if (pitch == 0x2C) {
+  } else if (key == 0x2C) {
     period = 300.99;
-  } else if (pitch == 0x2D) {
+  } else if (key == 0x2D) {
     period = 284.09;
-  } else if (pitch == 0x2E) {
+  } else if (key == 0x2E) {
     period = 268.15;
-  } else if (pitch == 0x2F) {
+  } else if (key == 0x2F) {
     period = 253.1;
-  } else if (pitch == 0x30) {
+  } else if (key == 0x30) {
     period = 238.89;
-  } else if (pitch == 0x31) {
+  } else if (key == 0x31) {
     period = 225.49;
-  } else if (pitch == 0x32) {
+  } else if (key == 0x32) {
     period = 212.82;
-  } else if (pitch == 0x33) {
-    period = 188.75;
-  } else if (pitch == 0x34) {
+  } else if (key == 0x33) {
+    period = 201;
+  } else if (key == 0x34) {
     period = 189.61;
-  } else if (pitch == 0x35) {
+  } else if (key == 0x35) {
     period = 178.97;
-  } else if (pitch == 0x36) {
+  } else if (key == 0x36) {
     period = 168.92;
-  } else if (pitch == 0x37) {
+  } else if (key == 0x37) {
     period = 159.44;
-  } else if (pitch == 0x38) {
+  } else if (key == 0x38) {
     period = 150.49;
-  } else if (pitch == 0x39) {
+  } else if (key == 0x39) {
     period = 142.05;
-  } else if (pitch == 0x3A) {
+  } else if (key == 0x3A) {
     period = 134.07;
-  } else if (pitch == 0x3B) {
+  } else if (key == 0x3B) {
     period = 126.55;
-  } else if (pitch == 0x3C) {
+  } else if (key == 0x3C) {
     period = 119.45;
-  } else if (pitch == 0x3D) {
+  } else if (key == 0x3D) {
     period = 112.74;
-  } else if (pitch == 0x3E) {
+  } else if (key == 0x3E) {
     period = 106.41;
-  } else if (pitch == 0x3F) {
+  } else if (key == 0x3F) {
     period = 100.44;
-  } else if (pitch == 0x40) {
+  } else if (key == 0x40) {
     period = 94.8;
-  } else if (pitch == 0x41) {
+  } else if (key == 0x41) {
     period = 89.48;
-  } else if (pitch == 0x42) {
+  } else if (key == 0x42) {
     period = 84.46;
-  } else if (pitch == 0x43) {
+  } else if (key == 0x43) {
     period = 79.72;
-  } else if (pitch == 0x44) {
+  } else if (key == 0x44) {
     period = 75.25;
-  } else if (pitch == 0x45) {
+  } else if (key == 0x45) {
     period = 71.02;
-  } else if (pitch == 0x46) {
+  } else if (key == 0x46) {
     period = 67.04;
-  } else if (pitch == 0x47) {
+  } else if (key == 0x47) {
     period = 63.27;
-  } else if (pitch == 0x48) {
+  } else if (key == 0x48) {
     period = 59.72;
-  } else if (pitch == 0x49) {
+  } else if (key == 0x49) {
     period = 56.37;
-  } else if (pitch == 0x4A) {
+  } else if (key == 0x4A) {
     period = 53.21;
-  } else if (pitch == 0x4B) {
+  } else if (key == 0x4B) {
     period = 50.22;
-  } else if (pitch == 0x4C) {
+  } else if (key == 0x4C) {
     period = 47.4;
-  } else if (pitch == 0x4D) {
+  } else if (key == 0x4D) {
     period = 44.74;
-  } else if (pitch == 0x4E) {
+  } else if (key == 0x4E) {
     period = 42.23;
-  } else if (pitch == 0x4F) {
+  } else if (key == 0x4F) {
     period = 39.86;
-  } else if (pitch == 0x50) {
+  } else if (key == 0x50) {
     period = 37.62;
-  } else if (pitch == 0x51) {
+  } else if (key == 0x51) {
     period = 35.51;
-  } else if (pitch == 0x52) {
+  } else if (key == 0x52) {
     period = 33.52;
-  } else if (pitch == 0x53) {
+  } else if (key == 0x53) {
     period = 31.64;
-  } else if (pitch == 0x54) {
+  } else if (key == 0x54) {
     period = 29.86;
   }
   return period;
